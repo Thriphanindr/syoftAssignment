8ae3b59c90e11d6b1e5cb750e30beaf148951651
Thriphanindra <phanindra0041@gmail.com>
2024-07-31 22:54:08 +0530

    Assignment

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const { authenticate, authorize } = require('../middleware/auth');
 
-router.post('/', authenticate, authorize('admin'), productController.createProduct);
+router.post('/', authenticate, authorize('admin','staff'), productController.createProduct);
 router.get('/', authenticate, authorize('admin', 'manager'), productController.getProducts);
 router.put('/:id', authenticate, authorize('admin', 'manager'), productController.updateProduct);
 router.delete('/:id', authenticate, authorize('admin'), productController.deleteProduct);
